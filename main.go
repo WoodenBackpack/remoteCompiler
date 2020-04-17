829b70a9f1548f2d20d1a3f488e97e5d05c6f711
WoodenBackpack <piotrwrobl90@gmail.com>
2020-04-17 08:45:39 +0200

    grouped code in functions

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
+
 import(
 	"fmt"
 	"net"
 	"os/exec"
 	"os"
-	"time"
 )
 
 func getPortForListening() (net.PacketConn, error) {
@@ -17,15 +17,15 @@ func getPortForListening() (net.PacketConn, error) {
 	return pc, nil
 }
 
-func receivePacket(pc net.PacketConn) ([]byte, error) {
+func receivePacket(pc net.PacketConn) ([]byte, net.Addr, error) {
 	buf := make([]byte, 1024)
 	_, addr, err := pc.ReadFrom(buf)
 	if (err != nil) {
-		return nil, err
+		return nil, nil, err
 	}
 	fmt.Printf("received packet!\n")
 	pc.WriteTo(buf, addr)
-	return buf, nil
+	return buf, addr, nil
 }
 
 type FileType string
@@ -33,6 +33,7 @@ type FileType string
 const (
 	CppFile FileType = "CppFile"
 	PythonFile = "PythonFile"
+	LOCAL_VLAN_SOCKET_ADDRESS string = "10.1.137.61:8001"
 )
 
 func createFileOfTypeFromBuffer(fType FileType, content []byte) (*os.File, error) {
@@ -55,61 +56,66 @@ func createFileOfTypeFromBuffer(fType FileType, content []byte) (*os.File, error
 	return file, nil
 }
 
-func main() {
-	listenConfig, err := getPortForListening()
-	if err != nil {
-		fmt.Printf("err: %s\n", err)
-		return
+func compileMainAndRun() (string) {
+	stdOut, errStr := exec.Command("g++", "-g", "main.cpp", "-oout").CombinedOutput()
+	var output string
+	if errStr != nil {
+		output += "compilation status:\n" + errStr.Error() + "\n"
+		output = "compilation errors:\n" + string(stdOut) + "\n"
+	} else {
+		output = "compilation success !\n"
 	}
-	for {
-		packet, err := receivePacket(listenConfig)
-		if (err != nil) {
-			fmt.Printf("error with receiving packets: %s\n", err)
-			return
-		}
-		file, err := createFileOfTypeFromBuffer(CppFile, packet)
-                if err == nil {
-			out, errStr := exec.Command("g++", "main.cpp", "-oout").CombinedOutput()
-			if errStr != nil {
-			    fmt.Printf("stdErr:\n%s\n", errStr)
-			}
-			execOut, errExecStr := exec.Command("./out").CombinedOutput()
-			if errStr == nil {
-			    fmt.Printf("stdOutput:\n%s\n", execOut)
-			} else {
-			    fmt.Printf("stdErr:\n%s\n", errExecStr)
-			}
-			time.Sleep(2 * time.Second)
-
-			responseAddr, err := net.ResolveUDPAddr("udp", "10.1.231.67:8001")
-			if err != nil {
-				fmt.Printf("cannot resolve addr! %s", err)
-			}
-			localAddr, _ := net.ResolveUDPAddr("idp", "10.1.137.61:8001")
-			connection, errorConnection := net.DialUDP("udp", localAddr ,responseAddr)
-		        if (errorConnection != nil) {
-			    fmt.Printf("error in listening! %s\n", errorConnection)
-		        }
-			fmt.Printf("addr: ip=%s, port=%d\n", responseAddr.IP, responseAddr.Port)
-			fmt.Printf("data = %s\n", out)
-				_, errorWriter := connection.Write(out)
-		                if (errorWriter != nil) {
-		                        fmt.Printf("error in writing! %s\n", errorWriter)
-	                        }
-		//	}
-			fmt.Printf("successfully received and sent pacekt!\n")
-		} else {
-			fmt.Printf("cant write to file err: %s", err)
-		}
-		file.Close()
-
-		os.Remove("main.cpp")
-		os.Remove("out")
+	execOut, errExecStr := exec.Command("./out").CombinedOutput()
+	if errStr == nil {
+	    //output += "Execution output:\n" + string(execOut) + "\n"
+	    gdbOut, _ := exec.Command("gdb", "-batch", "-ex", "run", "-ex", "bt", "./out").CombinedOutput()
+	    output += string(gdbOut)
+	} else {
+	    output += "Execution err output:\n" + errExecStr.Error() + "\n"
+	    output += "Execution output:\n" + string(execOut) + "\n"
 	}
+	return output
 }
 
+func sendResponseBack(address net.Addr, output string) {
+  responseAddr, err := net.ResolveUDPAddr("udp", address.String())
+  if err != nil {
+      fmt.Printf("cannot resolve addr! %s", err)
+  }
+  localAddr, _ := net.ResolveUDPAddr("udp", LOCAL_VLAN_SOCKET_ADDRESS)
+  responseAddr.Port = 8001
+  connection, errorConnection := net.DialUDP("udp", localAddr ,responseAddr)
+  if (errorConnection != nil) {
+      fmt.Printf("error in listening! %s\n", errorConnection)
+  }
+  _, errorWriter := connection.Write([]byte(output))
+  if (errorWriter != nil) {
+      fmt.Printf("error in writing! %s\n", errorWriter)
+  }
+  connection.Close()
+}
 
-
-
-
-
+func main() {
+  listenConfig, err := getPortForListening()
+  if err != nil {
+    fmt.Printf("err: %s\n", err)
+    return
+  }
+  for {
+    packet, addr, err := receivePacket(listenConfig)
+    if (err != nil) {
+      fmt.Printf("error with receiving packets: %s\n", err)
+      return
+    }
+    file, err := createFileOfTypeFromBuffer(CppFile, packet)
+    if err == nil {
+      output := compileMainAndRun()
+      sendResponseBack(addr, output)
+    } else {
+      fmt.Printf("cant write to file err: %s", err)
+    }
+    file.Close()
+    os.Remove("main.cpp")
+    os.Remove("out")
+  }
+}
